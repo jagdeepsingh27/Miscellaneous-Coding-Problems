86fad6b90ee33d4378eae50cb2af8c77776414d3
Jagdeep Singh <132729448+jagdeepsingh27@users.noreply.github.com>
2023-07-03 23:29:18 +0530

    adds find factorial program

---
+++ cpp/recursion/1-Factorial.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+#include <string>
+
+using namespace std;
+
+int findFactorial(int);
+
+int main(int argc, char const *argv[])
+{
+    
+    // press y for continue and n for exit
+    string isExit;
+    int input;
+  
+    do{
+    
+        cout << "FIND FACTORIAL OF NUMBER  \n";
+        cout << "Type a NUMBER to find factorial & Press ENTER \n";
+        cin >> input; 
+
+
+        int result = findFactorial(input);
+       
+        cout << "facotrial of " << input << " is "<< result <<  "\n";
+
+        cout << "PRESS 'Q' for Exit \n";
+        cin >> isExit;   
+
+    }while(isExit != "q" || isExit != "Q"); 
+    
+
+    return 0;
+}
+
+int findFactorial(int number){
+    int fact = 1;
+    if(number == 1){
+        return 1;
+    }else{
+        fact = number * findFactorial(number  -1);
+    }
+    return fact;
+}
